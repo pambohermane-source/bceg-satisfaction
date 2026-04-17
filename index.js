8219b5ba88348dc905aed1060812a08482faa871
pambohermane-source <pambohermane@gmail.com>
2026-04-17 22:23:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Base de donnees
 require('./models/database');
 
-// Routes
 app.use('/enquete', require('./routes/enquete'));
 app.use('/dashboard', require('./routes/dashboard'));
-app.use('/excel', require('./routes/excel'));
-app.use('/sms', require('./routes/sms'));
 
-// Redirection racine vers dashboard
-app.get('/', (req, res) => res.redirect('/dashboard'));
+try {
+  app.use('/excel', require('./routes/excel'));
+} catch(e) { console.log('Route excel non disponible:', e.message); }
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+try {
+  app.use('/sms', require('./routes/sms'));
+} catch(e) { console.log('Route sms non disponible:', e.message); }
+
+app.get('/', function(req, res) { res.redirect('/dashboard'); });
+
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
   console.log('Serveur BCEG demarre sur le port ' + PORT);
 });
