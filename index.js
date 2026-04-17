d2860df2993c585dcb2a5a929687efa43de81e0d
pambohermane-source <pambohermane@gmail.com>
2026-04-17 15:09:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
-
-const db = require('./models/database');
-const excelRoutes = require('./routes/excel');
-const enqueteRoutes = require('./routes/enquete');
-const dashboardRoutes = require('./routes/dashboard');
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.redirect('/dashboard');
-});
+// Base de donnees
+require('./models/database');
 
-app.use('/excel', excelRoutes);
-app.use('/enquete', enqueteRoutes);
-app.use('/dashboard', dashboardRoutes);
+// Routes
+const enqueteRouter = require('./routes/enquete');
+const dashboardRouter = require('./routes/dashboard');
+const excelRouter = require('./routes/excel');
+const smsRouter = require('./routes/sms');
 
+app.use('/enquete', enqueteRouter);
+app.use('/dashboard', dashboardRouter);
+app.use('/excel', excelRouter);
+app.use('/sms', smsRouter);
+
+// Redirection racine vers dashboard
+app.get('/', (req, res) => res.redirect('/dashboard'));
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Serveur BCEG démarré sur http://localhost:${PORT}`);
+  console.log(`Serveur BCEG demarre sur le port ${PORT}`);
+  console.log(`Dashboard : http://localhost:${PORT}/dashboard`);
 });
